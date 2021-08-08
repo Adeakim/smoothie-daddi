f300a1923255e24e83d03a60af6f7e8a466b5bd3
Decagon <decagon@Hakeems-MacBook-Air.local>
2021-08-08 15:21:08 +0100

    refactoring to collect staticfiles

--- smoothiedaddy/settings.py
+++ smoothiedaddy/settings.py
@@ -141,7 +141,7 @@ MEDIA_URL = '/smoothie/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR,"static")
 ]
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+# STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 
 # MEDIA_ROOT = os.path.join(BASE_DIR, 'static/images')
